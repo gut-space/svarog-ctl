14e2a40b0aa4ec422fa1d129a8d286e7cf9df3a7
Tomek Mrugalski <tomasz@isc.org>
2021-06-17 23:06:41 +0200

    Initial repo layout

---
+++ README.md
@@ -0,0 +1,8 @@
+# svarog-ctl
+
+Svarog-ctl is a command-line driven automated satellite tracking software
+that uses `rotctld` to control antenna rotors to track satellites fly overs.
+
+## Status
+
+The software was just stared, it doesn't work yet.
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="svarog_ctl",
+    version="0.0.0",
+    author="Tomek Mrugalski",
+    author_email="spam.python@klub.com.pl",
+    description="Automated satellite tracker that controls antenna rotors via rotctld",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gut-space/analog-noise-estimator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["orbit-predictor"],
+    python_requires='>=3.6',
+)

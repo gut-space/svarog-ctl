59ae542a00caea69a5789571df9e0d8078513b9b
Tomek Mrugalski <tomasz.mrugalski@eti.pg.edu.pl>
2021-06-20 17:57:17 +0200

    utils/globalvars.py (logging config) added

---
+++ utils/globalvars.py
@@ -0,0 +1,61 @@
+import os
+import logging
+import yaml
+
+DEV_ENVIRONMENT =  os.environ.get("DEV_ENVIRONMENT") is not None
+APP_NAME = "svarog-ctl"
+VERSION = "0.1.0"
+
+CONFIG_DIRECTORY: str = os.environ.get("SVAROG_CONFIG_DIR") # type: ignore
+if CONFIG_DIRECTORY is None:
+    if DEV_ENVIRONMENT:
+        CONFIG_DIRECTORY = os.path.abspath("./config")
+    else:
+        CONFIG_DIRECTORY = os.path.expanduser("~/.config/%s" % (APP_NAME,))
+
+CONFIG_PATH = os.path.join(CONFIG_DIRECTORY, "config.yml")
+LOG_FILE = os.path.join(CONFIG_DIRECTORY, APP_NAME + ".log") if not DEV_ENVIRONMENT else None
+
+if not os.path.exists(CONFIG_DIRECTORY):
+    os.makedirs(CONFIG_DIRECTORY, exist_ok=True)
+
+# Loglevel is a bit complicated. By default, it's INFO, unless it's set in the config file,
+# unless it's a dev environment, then it's DEBUG.
+LOGLEVEL = logging.INFO
+
+SHORT_LOG = True
+
+try:
+    with open(CONFIG_PATH) as f:
+        config = yaml.safe_load(f) # type: ignore
+        lv = config["logging"]["level"]
+        LOGLEVEL = logging._nameToLevel[lv]
+except:
+    pass
+
+try:
+    with open(CONFIG_PATH) as f:
+        config = yaml.safe_load(f) # type: ignore
+        LOG_FILE = os.path.expanduser(config["logging"]["file"])
+except:
+    pass
+
+if (LOG_FILE == "stdout"):
+    LOG_FILE = None
+
+print("Logging on level %s to file %s" % (logging.getLevelName(LOGLEVEL), LOG_FILE))
+
+if DEV_ENVIRONMENT:
+    LOGLEVEL = logging.DEBUG
+
+if SHORT_LOG:
+    format='%(asctime)s %(levelname)7s: %(message)s'
+    datefmt='%H:%M:%S'
+else:
+    format = '%(asctime)s %(levelname)s %(filename)s:%(lineno)d: %(message)s'
+    datefmt = None # This will use the default ‘%Y-%m-%d %H:%M:%S,uuu’
+
+logging.basicConfig(level = LOGLEVEL,
+                    format=format,
+                    datefmt=datefmt,
+                    filename=LOG_FILE)

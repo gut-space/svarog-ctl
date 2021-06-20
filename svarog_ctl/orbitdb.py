3090c5b83428a71414a56d286d07c550f8633f0a
Tomek Mrugalski <tomasz.mrugalski@eti.pg.edu.pl>
2021-06-20 18:38:17 +0200

    Imported couple files from perylune
     (which got it imported from old svarog code)

--- svarog_ctl/orbitdb.py
+++ svarog_ctl/orbitdb.py
@@ -9,27 +9,45 @@ import time
 from orbit_predictor.sources import NoradTLESource
 from orbit_predictor.predictors.base import CartesianPredictor
 
-from utils.globalvars import CONFIG_DIRECTORY, APP_NAME
+from svarog_ctl.tle import tle
+
+from utils.globalvars import CONFIG_DIRECTORY, APP_NAME, VERSION
 from utils.configuration import open_config
-from utils.filenames import safe_filename
+from utils.utils import url_to_filename
+
 
 CELESTRAK = [
-    r"https://celestrak.com/NORAD/elements/active.txt"
+    r"https://celestrak.com/NORAD/elements/active.txt",
+    "file://local.txt"
 ]
 
 class OrbitDatabase:
     def __init__(self, urls=None, max_period=7*24*60*60):
         self.max_period = max_period
         if urls is None:
-            config = open_config()
-            urls = config['norad']
-        if urls is None:
             urls = CELESTRAK
-        self.urls: Sequence[str]
         self.urls = urls
 
+        self.tle_names = {}
+        self.tle_norad = {}
+
+        # Store all information in the ${DATADIR}/tle directory.
+        cfg = open_config()
+        logging.debug("Loaded config: %s" % repr(cfg))
+        self.datadir = os.path.join(cfg['datadir'], 'tle')
+
     def _get_tle_from_url(self, url):
-        headers = {'user-agent': APP_NAME, 'Accept': 'text/plain'}
+        if (url[:7] == "file://"):
+            fname = self.datadir + os.path.sep + url[7:]
+            logging.debug("Reading file [%s]" % fname)
+
+            with open(fname, "r") as f:
+                content = f.read()
+                f.close()
+                logging.debug("Loaded %d bytes from file %s" % (len(content), fname))
+                return content
+
+        headers = { 'user-agent': APP_NAME + " " + VERSION, 'Accept': 'text/plain' }
         try:
             response = requests.get(url, headers=headers)
         except requests.exceptions.RequestException as error:
@@ -38,14 +56,17 @@ class OrbitDatabase:
         return response.content.decode("UTF-8")
 
     def _fetch_tle_and_save(self, url, tle_path):
+        logging.info("Downloading %s to local file %s" % (url, tle_path))
+
         content = self._get_tle_from_url(url)
         with open(tle_path, "w") as f:
             f.write(content)
         return tle_path
 
     def _get_tle_path_from_url(self, url):
-        tle_filename = safe_filename(url)
-        tle_path = os.path.join(CONFIG_DIRECTORY, tle_filename)
+        tle_filename = url_to_filename(url)
+
+        tle_path = os.path.join(self.datadir, tle_filename)
         return tle_path
 
     def _get_create_time(self, path):
@@ -54,7 +75,7 @@ class OrbitDatabase:
         return ctime
 
     def _is_out_of_date(self, path):
-        ctime = self._get_create_time(path)    
+        ctime = self._get_create_time(path)
         now = time.time()
         return now > ctime + self.max_period
 
@@ -62,6 +83,7 @@ class OrbitDatabase:
         tle_path = self._get_tle_path_from_url(url)
         tle_path_exists = os.path.exists(tle_path)
         if not force_fetch and tle_path_exists and not self._is_out_of_date(tle_path):
+            logging.info("%s is up-to-date, skipping download." % tle_path)
             return tle_path
 
         try:
@@ -79,60 +101,90 @@ class OrbitDatabase:
         except LookupError:
             return False
 
-    def _get_source(self, sat_id):
+    def get_predictor(self, sat_id) -> CartesianPredictor:
         for url in self.urls:
             path = self._get_current_tle_file(url)
             source = NoradTLESource.from_file(path)
             if self._is_in_source(source, sat_id):
-                return source
+                return source.get_predictor(sat_id)
         raise LookupError("Could not find %s in orbit data." % (sat_id,))
-
-    def get_predictor(self, sat_id: str) -> CartesianPredictor:
-        source = self._get_source(sat_id)
-        return source.get_predictor(sat_id)
-
-    def get_tle(self, sat_id: str, date: datetime.datetime) -> List[str]:
-        source = self._get_source(sat_id)
-        return source.get_tle(sat_id, date).lines # type: ignore
 
     def refresh_satellites(self, sat_ids):
         all_sat_ids = set(sat_ids)
         found_sat_ids = set()
         for url in self.urls:
-            satellites_to_search = all_sat_ids.difference(found_sat_ids)
-            if len(satellites_to_search) == 0:
+            sats_to_search = all_sat_ids.difference(found_sat_ids)
+            if len(sats_to_search) == 0:
                 return
 
             path = self._get_current_tle_file(url, force_fetch=True)
             source = NoradTLESource.from_file(path)
-            
-            for sat_id in satellites_to_search:
+
+            for sat_id in sats_to_search:
                 if self._is_in_source(source, sat_id):
-                    found_sat_ids.add(sat_id)        
+                    found_sat_ids.add(sat_id)
 
         if all_sat_ids != found_sat_ids:
             raise LookupError("Could not find %s in orbit data." % (", ".join(all_sat_ids.difference(found_sat_ids))))
-    
-    def refresh_urls(self):
+
+    def refresh_urls(self, force_fetch = False):
+        """Downloads all defined TLE information from Celestrak and other defined sources"""
         urls = self.urls
 
         for url in urls:
-            self._get_current_tle_file(url, force_fetch=True)
+            self._get_current_tle_file(url, force_fetch=force_fetch)
+            self.parse_tlebulk(self._get_tle_path_from_url(url))
+
+    def parse_all(self):
+        for url in self.urls:
+            path = self._get_current_tle_file(url)
+            self.parse_tlebulk(path)
+
+    def parse_tlebulk(self, file: str = None):
+        """Parses loaded TLE data, as downloaded from CELESTRAK. The file is essentially a
+           lot of TLE lines concatenated together."""
+
+        cnt = 0
+        with open(file) as f:
+            lines = f.readlines()
+        for i in range(int(len(lines) / 3) ):
+            name = lines[3*i].strip()
+            line1 = lines[3*i+1].strip()
+            line2 = lines[3*i+2].strip()
+            t = tle(line1, line2, name)
+            self.tle_names[name] = t
+            self.tle_norad[t.norad] = t
+            cnt += 1
+        logging.info("Loaded %d TLEs." % cnt)
+
+    def get_name(self, l: str) -> tle:
+        """Attempts to return a TLE by its name, e.g. get_name("NOAA 18") """
+        return self.tle_names[l]
+
+    def get_norad(self, l: int) -> tle:
+        """Attempts to return a TLE by its norad number, e.g. get_name(12345) """
+        return self.tle_norad[l]
+
+    def count(self) -> int:
+        """Returns number of currently loaded TLEs"""
+        return len(self.tle_norad)
 
     def __str__(self):
+        """This method is used when printing the orbitdb object"""
         data = []
         for url in self.urls:
             path = self._get_tle_path_from_url(url)
             exists = os.path.exists(path)
+            logging.debug("DEBUG: path=%s, url=%s" % (path, url) )
             if exists:
                 out_of_date = self._is_out_of_date(path)
                 creation_time = self._get_create_time(path)
                 now = time.time()
                 age = now - creation_time
-                
+
                 dt = datetime.timedelta(seconds=age)
                 data.append((url, "%s: %s ago" % ("Out-of-date" if out_of_date else "Current", str(dt))))
             else:
                 data.append((url, "Not exists"))
-        
+
         return "\n".join("%s - %s" % (url, desc) for url, desc in data)

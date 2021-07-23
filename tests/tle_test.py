d594ca6b30963c49f07c0c44e6b8d36ba8cc8892
Tomek Mrugalski <tomasz.mrugalski@eti.pg.edu.pl>
2021-07-24 00:30:12 +0200

    All tests are now using unittest

--- tests/tle_test.py
+++ tests/tle_test.py
@@ -1,51 +1,54 @@
 from svarog_ctl.tle import Tle
 import pytest
+import unittest
 
 NAME='NOAA 15'
 LINE1='1 25338U 98030A   19351.71640046 +.00000015 +00000-0 +24973-4 0  9993'
 LINE2='2 25338 098.7340 012.5392 0011411 075.8229 284.4218 14.25943731122932'
 
-def test_tle_bad_init():
+class TleTest(unittest.TestCase):
 
-    with pytest.raises(TypeError):
-        # without any strings
-        Tle()
+    def test_tle_bad_init(self):
 
-    with pytest.raises(TypeError):
-        # with just one line
-        Tle(LINE1)
+        with pytest.raises(TypeError):
+            # without any strings
+            Tle()
 
-    with pytest.raises(TypeError):
-        Tle(LINE1, LINE2, NAME, "unsubstantiated nonsense")
+        with pytest.raises(TypeError):
+            # with just one line
+            Tle(LINE1)
 
-def test_tle2lines():
+        with pytest.raises(TypeError):
+            Tle(LINE1, LINE2, NAME, "unsubstantiated nonsense")
 
-    x = Tle(LINE1, LINE2)
-    assert x.name == ""
-    assert x.line1 == LINE1
-    assert x.line2 == LINE2
+    def test_tle2lines(self):
 
-def test_tle3lines():
+        x = Tle(LINE1, LINE2)
+        assert x.name == ""
+        assert x.line1 == LINE1
+        assert x.line2 == LINE2
 
-    x = Tle(LINE1, LINE2, NAME)
-    assert x.name == NAME
-    assert x.line1 == LINE1
-    assert x.line2 == LINE2
+    def test_tle3lines(self):
 
-def test_tle_parse():
-    x = Tle(LINE1, LINE2, NAME)
-    assert x.norad == 25338
+        x = Tle(LINE1, LINE2, NAME)
+        assert x.name == NAME
+        assert x.line1 == LINE1
+        assert x.line2 == LINE2
 
-def test_tle_repr():
-    """Make sure there's an easy way to print the Tle"""
-    x = Tle(LINE1, LINE2, NAME)
+    def test_tle_parse(self):
+        x = Tle(LINE1, LINE2, NAME)
+        assert x.norad == 25338
 
-    exp = NAME + '\n' + LINE1 + '\n' + LINE2
-    assert x.__str__() == exp
-    assert repr(x) == exp
+    def test_tle_repr(self):
+        """Make sure there's an easy way to print the Tle"""
+        x = Tle(LINE1, LINE2, NAME)
 
-def test_tle_parsing():
-    x = Tle(LINE1, LINE2, NAME)
+        exp = NAME + '\n' + LINE1 + '\n' + LINE2
+        assert x.__str__() == exp
+        assert repr(x) == exp
 
-    assert x.get_id() == 25338
-    assert x.get_name() == "NOAA 15"
+    def test_tle_parsing(self):
+        x = Tle(LINE1, LINE2, NAME)
+
+        assert x.get_id() == 25338
+        assert x.get_name() == "NOAA 15"

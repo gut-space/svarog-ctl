f70e91a45006a7c727ef08d654134ed1903e7aa1
Tomek Mrugalski <tomasz.mrugalski@eti.pg.edu.pl>
2021-07-24 00:30:12 +0200

    All tests are now using unittest

--- tests/orbitdb_test.py
+++ tests/orbitdb_test.py
@@ -1,73 +1,77 @@
 from svarog_ctl import orbitdb
 from svarog_ctl import tle
 import pytest
+import unittest
 
-def test_db_stats():
+class PassesTest(unittest.TestCase):
 
-    db = orbitdb.OrbitDatabase()
 
-    # Nothing is loaded by default
-    assert db.count() == 0
+    def test_db_stats(self):
 
-    # There's close to 2600 sats listed for now, but the number may fluctuate over
-    # time. Let's make the test robust.
-    db.refresh_urls()
-    assert db.count() > 2500
+        db = orbitdb.OrbitDatabase()
 
-def test_load_db():
+        # Nothing is loaded by default
+        assert db.count() == 0
 
-    db = orbitdb.OrbitDatabase()
-    db.refresh_urls()
+        # There's close to 2600 sats listed for now, but the number may fluctuate over
+        # time. Let's make the test robust.
+        db.refresh_urls()
+        assert db.count() > 2500
 
-    tle1 = db.get_name("NOAA 18")
-    tle2 = db.get_norad(28654)
+    def test_load_db(self):
 
-    assert tle1 is not None
-    assert isinstance(tle1, tle.Tle)
-    assert tle1.name == "NOAA 18"
-    assert tle1.norad == 28654
+        db = orbitdb.OrbitDatabase()
+        db.refresh_urls()
 
-    assert tle2 is not None
-    assert isinstance(tle2, tle.Tle)
-    assert tle2.name == "NOAA 18"
-    assert tle2.norad == 28654
+        tle1 = db.get_name("NOAA 18")
+        tle2 = db.get_norad(28654)
 
-    with pytest.raises(KeyError):
-        db.get_name("nonexistent")
+        assert tle1 is not None
+        assert isinstance(tle1, tle.Tle)
+        assert tle1.name == "NOAA 18"
+        assert tle1.norad == 28654
 
-    with pytest.raises(KeyError):
-        db.get_norad(1234567)
+        assert tle2 is not None
+        assert isinstance(tle2, tle.Tle)
+        assert tle2.name == "NOAA 18"
+        assert tle2.norad == 28654
 
-    pred = db.get_predictor("NOAA 18")
-    assert pred
+        with pytest.raises(KeyError):
+            db.get_name("nonexistent")
 
-    assert db.get_name_by_norad(28654) == "NOAA 18"
+        with pytest.raises(KeyError):
+            db.get_norad(1234567)
 
-def test_add_custom():
+        pred = db.get_predictor("NOAA 18")
+        assert pred
 
-    LINE1 = "1 44427U 98067QM  21192.54020985  .00022355  00000-0  19763-3 0  9995"
-    LINE2 = "2 44427  51.6376 177.8799 0003618 359.5888  93.1405 15.68562202115256"
-    NAME = "KRAKSAT"
+        assert db.get_name_by_norad(28654) == "NOAA 18"
 
-    db = orbitdb.OrbitDatabase()
+    def test_add_custom(self):
 
-    # Make sure there's no entry for KRAKSAT
-    with pytest.raises(KeyError):
-        db.get_name("KRAKSAT")
-    with pytest.raises(KeyError):
-        db.get_norad(44427)
-    with pytest.raises(KeyError):
-        db.get_name_by_norad(44427)
+        LINE1 = "1 44427U 98067QM  21192.54020985  .00022355  00000-0  19763-3 0  9995"
+        LINE2 = "2 44427  51.6376 177.8799 0003618 359.5888  93.1405 15.68562202115256"
+        NAME = "KRAKSAT"
 
-    # Now add a custom TLE
-    db.add_tle(LINE1, LINE2, NAME)
+        db = orbitdb.OrbitDatabase()
 
-    tle1 = db.get_name(NAME)
-    tle2 = db.get_norad(44427)
+        # Make sure there's no entry for KRAKSAT
+        with pytest.raises(KeyError):
+            db.get_name("KRAKSAT")
+        with pytest.raises(KeyError):
+            db.get_norad(44427)
+        with pytest.raises(KeyError):
+            db.get_name_by_norad(44427)
 
-    assert tle1 is not None
-    assert tle2 is not None
-    assert isinstance(tle1, tle.Tle)
-    assert isinstance(tle2, tle.Tle)
+        # Now add a custom TLE
+        db.add_tle(LINE1, LINE2, NAME)
 
-    assert db.get_name_by_norad(44427) == "KRAKSAT"
+        tle1 = db.get_name(NAME)
+        tle2 = db.get_norad(44427)
+
+        self.assertIsNotNone(tle1)
+        self.assertIsNotNone(tle2)
+        assert isinstance(tle1, tle.Tle)
+        assert isinstance(tle2, tle.Tle)
+
+        assert db.get_name_by_norad(44427) == "KRAKSAT"
--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,18 +1,24 @@
 from svarog_ctl import utils
 
-def test_url_to_filename():
-    # Tests if URL can be properly converted to a filename
-    values = [ ['https://celestrak.com/NORAD/elements/active.txt', 'celestrak.com-NORAD-elements-active.txt'],
-               ['ftp://isc.org/foo.txt', 'isc.org-foo.txt'] ]
+import unittest
 
-    for _, row in enumerate(values):
-        assert utils.url_to_filename(row[0]) == row[1]
 
-def test_coords():
-    test_data = [ [ 54.0, 19.0, None, "54.0000N 19.0000E"],
-                  [-54.0, 19.0, None, "54.0000S 19.0000E"],
-                  [-54.0,-19.0, None, "54.0000S 19.0000W"],
-                  [ 54.0, 19.0, 123,  "54.0000N 19.0000E 123m"] ]
+class UtilsTest(unittest.TestCase):
 
-    for case in test_data:
-        assert utils.coords(case[0], case[1], case[2]) == case[3]
+
+    def test_url_to_filename(self):
+        # Tests if URL can be properly converted to a filename
+        values = [ ['https://celestrak.com/NORAD/elements/active.txt', 'celestrak.com-NORAD-elements-active.txt'],
+                ['ftp://isc.org/foo.txt', 'isc.org-foo.txt'] ]
+
+        for _, row in enumerate(values):
+            assert utils.url_to_filename(row[0]) == row[1]
+
+    def test_coords(self):
+        test_data = [ [ 54.0, 19.0, None, "54.0000N 19.0000E"],
+                    [-54.0, 19.0, None, "54.0000S 19.0000E"],
+                    [-54.0,-19.0, None, "54.0000S 19.0000W"],
+                    [ 54.0, 19.0, 123,  "54.0000N 19.0000E 123m"] ]
+
+        for case in test_data:
+            assert utils.coords(case[0], case[1], case[2]) == case[3]

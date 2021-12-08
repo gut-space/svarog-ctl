64ce7ea818181026198e572e87b9f2cc2ce453a8
Tomek Mrugalski <tomek@isc.org>
2021-12-08 23:52:50 +0100

    Added proper tests for rotctld

--- tests/rotctld_test.py
+++ tests/rotctld_test.py
@@ -1,16 +1,23 @@
 import pytest
+import subprocess
 from svarog_ctl.rotcltd import Rotctld
 import unittest
+import time
 
-class PassesTest(unittest.TestCase):
+class RotctldTest(unittest.TestCase):
+
+    rotctld_proc = None
 
     def setUp(self):
-        # TODO: need to start rotctld in the background with Dummy rotator
-        pass
+        # Start the rotctld with dummy rotor type 1 (-m 1) and run in verbose (-v) mode
+        CMD = 'rotctld -m 1 -t 45033 -v'
+        self.rotctld_proc = subprocess.Popen(CMD.split(' '))
+        time.sleep(1)
 
     def tearDown(self):
-        # TODO: kill the rotctld running in the background.
-        pass
+
+        self.rotctld_proc.send_signal(15) #sigterm
+        out, err = self.rotctld_proc.communicate(timeout=3) # try to kill it and wait 3 seconds.
 
     def test_ctor(self):
 
@@ -26,3 +33,11 @@ class PassesTest(unittest.TestCase):
 
         # This is clearly broken. It's supposed to be False before connect is called.
         self.assertFalse(r.connected())
+
+    def test_connect(self):
+        r = Rotctld("127.0.0.1", 45033)
+        m = r.connect()
+        self.assertEqual(m, "Dummy rotator") # make sure the rotctld reports the rotator type as dummy.
+                                             # That's because we started it with -m 1 (1 is a dummy rotator)
+
+        print(f"returned model: [{m}]")

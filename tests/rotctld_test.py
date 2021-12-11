45d59f9f6a3149bd06d8340921d70daf8ab649f7
Tomek Mrugalski <tomek@isc.org>
2021-12-11 21:10:03 +0100

    Many improvements in rotctld
     - az is now normalized to -180..180
     - added command for parking and getting capabilities
     - the functions for stop now return status
     - the logging errors now actually works
     - doc updated
     - tests added

--- tests/rotctld_test.py
+++ tests/rotctld_test.py
@@ -41,3 +41,38 @@ class RotctldTest(unittest.TestCase):
                                              # That's because we started it with -m 1 (1 is a dummy rotator)
 
         print(f"returned model: [{m}]")
+
+    def test_norm_azimuth(self):
+        """Tests if azimuth is normalized to -180..180 range."""
+        r = Rotctld("127.0.0.1", 45033)
+
+        for x in range(-200, 200, 10):
+            norm = r.norm_az(x)
+            self.assertTrue(norm >= -180.0 and norm <= 180)
+
+    def test_norm_elevation(self):
+        """Tests if elevation is normalized to 0..90 range."""
+        r = Rotctld("127.0.0.1", 45033)
+
+        for x in range(-200, 200, 10):
+            norm = r.norm_el(x)
+            self.assertTrue(norm >= 0 and norm <= 90)
+
+    def test_capabilities(self):
+        """Check if capabilities are returned. This test is only so-so, as the capabilities returned are
+           as plain text. There's no easy way to interpret this, so we chose a random properties returned,
+           check that they're in the returned text. There's a lot more returned there."""
+        r = Rotctld("127.0.0.1", 45033)
+        r.connect()
+        caps = r.capabilities()
+        r.close()
+
+        self.assertTrue(len(caps))
+
+        # strings we expect to see in the the response.
+        exp = [ "Caps dump for model:	1",
+                "Model name:		Dummy",
+                "Rot type:		Az-El"]
+
+        for e in exp:
+            self.assertTrue(caps.find(e) != -1)
